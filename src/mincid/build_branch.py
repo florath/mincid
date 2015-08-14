c5410f1ec03782e09e172c4cab20fbdba48cd725
Andreas Florath <andre@florath.net>
2015-08-14 09:19:34 +0200

    Fixed starrtup of branch creation script

--- src/mincid/build_branch.py
+++ src/mincid/build_branch.py
@@ -2,7 +2,18 @@
 #
 
 from MLogger import MLogger
+
+import os
 import sys
+
+class Branch(object):
+
+    def __init__(self, branch_name, tmp_dir):
+        self.__name = branch_name
+        self.__tmp_dir = tmp_dir
+        self.__branch_dir = os.path.join(tmp_dir, branch_name.replace("/", "_"))
+        self.__logger = MLogger("Branch", "build_branch", self.__branch_dir)
+        self.__logger.info("Init branch [%s]" % (self.__name))
 
 def main():
     branch = Branch(sys.argv[1], sys.argv[2])
--- src/mincid/build_branches_config.py
+++ src/mincid/build_branches_config.py
@@ -45,29 +45,30 @@ su - builder --command 'git clone %s %s'
             name_for_fs = branch_name.replace("/", "_")
 
             p.stdin.write(bytes(
-"""su - builder --command 'cd %s && git checkout %s && mkdir -p /artifacts/%s && cp mincid.json /artifacts/%s'
-""" %(self.__config['dest'], branch_name, name_for_fs, name_for_fs), 'UTF-8'))
+"""su - builder --command 'cd %s && git checkout %s && mkdir -p /artifacts/%s && cp mincid.json /artifacts/%s/mincid.json && chmod -R a+rwX /artifacts/%s'
+""" %(self.__config['dest'], branch_name, name_for_fs, name_for_fs, name_for_fs), 'UTF-8'))
 
         p.stdin.close()
         p.wait()
         self.__logger.debug("Docker process return value [%d]" % p.returncode)
-        
-#        for branch_name in self.__config['branches']:
-#            # Create Branch dir
-#            dirname = os.path.join(self.__tmp_dir, branch_name.replace("/", "_"))
-#            os.makedirs(dirname)
-#            stdouterr_filename = os.path.join(dirname, "sbatch.stdouterr")
-#            with open(stdouterr_filename, "w") as fd_stdouterr:
-#                p = subprocess.Popen(["sbatch", "--job-name=Project_%s" % self.__name,
-#                                      "--export=PYTHONPATH",
-#                                      os.path.join("/home/mincid/devel/mincid/src/mincid",
-#                                                   "build_branch.py"), branch_name, self.__tmp_dir],
-#                                     stdout=fd_stdouterr, stderr=fd_stdouterr)
-#        p.wait()
-#                                 
-#        self.__logger.info("sbatch process return value [%d]" % p.returncode)
-#        self.__logger.info("Finished project startup [%s]" % self.__name)
-   
+
+        # At this point for each branch there is a directory (in the tmp dir)
+        # that contains the configuration for the appropriate branch.
+        # Now start up the jobs (one for each branch) to handle these.
+        for branch_name in self.__config['branches']:
+            # Create Branch dir
+            dirname = os.path.join(self.__tmp_dir, branch_name.replace("/", "_"))
+            stdouterr_filename = os.path.join(dirname, "sbatch.stdouterr")
+            with open(stdouterr_filename, "w") as fd_stdouterr:
+                p = subprocess.Popen(["sbatch", "--job-name=Branch_%s" % branch_name,
+                                      "--export=PYTHONPATH",
+                                      os.path.join("/home/mincid/devel/mincid/src/mincid",
+                                                   "build_branch.py"), branch_name, self.__tmp_dir],
+                                     stdout=fd_stdouterr, stderr=fd_stdouterr)
+                p.wait()
+            self.__logger.info("sbatch process return value [%d]" % p.returncode)
+            self.__logger.info("Finished branch startup [%s]" % branch_name)
+        self.__logger.info("Finished complete branch startup")
   
 def main():
     bc = BranchesConfig(sys.argv[1])
